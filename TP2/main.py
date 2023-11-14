df202785926e85d8729b52e6d9d2939f38f68a0a
temPLAY333 <t.bourguet@alumno.um.edu.ar>
2023-11-13 23:47:13 -0300

    supercliente

--- TP2/main.py
+++ TP2/main.py
@@ -1,36 +1,31 @@
-import argparse, http.server, socketserver, socket, cgi, sys, os
+import argparse, http.server, socketserver, socket, time, cgi, sys, os
 from multiprocessing import Process, Queue
 from PIL import Image, ImageOps
 from http import HTTPStatus
 
 image_queue = Queue()
+response_queue = Queue()
 
+class MyTCPServer(socketserver.TCPServer):
+    address_family = socket.AF_INET6
 
 def image_processing_worker(queue):
     while True:
-        image_path, output_path, image_name, scale_factor = queue.get()
+        image_path, output_path, scale_factor = queue.get()
 
-        process_image(image_path, output_path)
-        print("Imagen Procesada")
+        grayscale_image = ImageOps.grayscale(Image.open(image_path))
+        grayscale_image.save(output_path)
 
         if scale_factor is not None:
             # Enviar tarea al servidor de redimensionamiento
             resized_image_data = send_to_resize_server(output_path, scale_factor)
-            output_path = os.path.join("output", "resized_" + image_name)
 
             # Guardar la imagen redimensionada
             with open(output_path, "wb") as resized_image_file:
                 resized_image_file.write(resized_image_data)
+            break
 
-
-def process_image(image_path, output_path):
-    print("Procesando imagen...")
-    original_image = Image.open(image_path)
-    grayscale_image = ImageOps.grayscale(original_image)
-
-    # Guardar la imagen en escala de grises
-    grayscale_image.save(output_path)
-
+    response_queue.put(output_path)
 
 def send_to_resize_server(image_path, scale_factor):
 
@@ -89,21 +84,25 @@ class ImageProcessingHandler(http.server.CGIHTTPRequestHandler):
                 print(f"El archivo {image_name} no existe en el directorio.")
 
             # Agregar tarea a la cola de procesamiento
-            output_filename = os.path.join("output", "gray_" + image_name)
-            image_queue.put((uploaded_filename, output_filename, image_name, scale_factor if resize_option == 'on' else None))
+            output_filename = os.path.join("output", "modified_" + image_name)
+            image_queue.put((uploaded_filename, output_filename, scale_factor if resize_option == 'on' else None))
+            
+            # Esperar a que se complete el procesamiento de imágenes
+            while not os.path.exists(output_filename):
+                time.sleep(1)
 
-            # Imagen para descargar - Refactorizar
-            # self.send_response(200)
-            # self.send_header('Content-type', 'application/octet-stream')
-            # self.send_header('Content-Disposition', f'attachment; filename="{os.path.basename(output_filename)}"')
-            # self.end_headers()
+            with open(output_filename, "rb") as image_file:
+                image_data = image_file.read()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/octet-stream')
+            self.send_header('Content-Disposition', f'attachment; filename="{os.path.basename(output_filename)}"')
+            self.end_headers()
+
+            self.wfile.write(image_data)
 
         except IOError as e:
             print(f"Error de E/S en el manejo del POST: {e}")
-            self.send_error(HTTPStatus.INTERNAL_SERVER_ERROR)
-
-        except OSError as e:
-            print(f"Error de sistema en el manejo del POST: {e}")
             self.send_error(HTTPStatus.INTERNAL_SERVER_ERROR)
 
         except http.server.CGIHTTPRequestHandler.CGIError as e:
@@ -118,8 +117,8 @@ def main(ip, port):
     image_worker.start()
 
     try:
-        handler = ImageProcessingHandler
-        with socketserver.TCPServer((ip, port), handler) as httpd:
+        server_class = MyTCPServer if ":" in ip else socketserver.TCPServer
+        with server_class((ip, port), ImageProcessingHandler) as httpd:
             print(f"Serving on {ip}:{port}")
             httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             httpd.serve_forever()
@@ -135,7 +134,7 @@ def main(ip, port):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="TP2 - procesa imágenes")
-    parser.add_argument("-i", "--ip", default="127.0.0.1", type=str, help="Dirección de escucha", required=True)
+    parser.add_argument("-i", "--ip", default="::1", type=str, help="Dirección de escucha", required=True)
     parser.add_argument("-p", "--port", default="7500", type=int, help="Puerto de escucha", required=True)
     args = parser.parse_args()
 
